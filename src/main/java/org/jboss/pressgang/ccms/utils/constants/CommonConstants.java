2859028fd9f1a3bd50e06ea2558b3e66f58499fd
Lee Newson <lnewson@redhat.com>
2013-11-12 15:51:24 +1000

    Added BZ#1012250 - RFE: Support <index>es

--- src/main/java/org/jboss/pressgang/ccms/utils/constants/CommonConstants.java
+++ src/main/java/org/jboss/pressgang/ccms/utils/constants/CommonConstants.java
@@ -240,4 +240,5 @@ public class CommonConstants {
     public static final String CS_ENTITIES_TITLE = "Entities";
     public static final String CS_SPACES_TITLE = "spaces";
     public static final String CS_DEFAULT_PUBLICAN_CFG_TITLE = "Default " + CS_PUBLICAN_CFG_TITLE;
+    public static final String CS_INDEX_TITLE = "Index";
 }
